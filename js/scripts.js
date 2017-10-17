0203215b1faafc386785e52566694004b053658f
danydacosta <danydc@comac.ch>
2017-10-17 10:44:28 +0200

    Creating JS classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
-// Empty JS for your own code to be here
+class Program{
+    constructor(city, map){
+        this.city = city
+        this.map = map
+    }
+
+    ShowMap(){
+
+    }
+
+    CloseMap(){
+
+    }
+
+    ShowPopup(){
+
+    }
+
+    ClosePopup(){
+
+    }
+}
+
+class City{
+    constructor(longitude, latitude, name){
+        this.longitude = longitude
+        this.latitude = latitude
+        this.name = name
+    }
+
+    FetchMeteo(){
+
+    }
+}
