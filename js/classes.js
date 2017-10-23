79433a1a83b269c210c9a6ac65925dba1996e032
danydacosta <danydc@comac.ch>
2017-10-23 14:32:19 +0200

    FetchTooltipWeather()

--- js/classes.js
+++ js/classes.js
@@ -8,27 +8,30 @@ class Program{
     Locate(){
         let that = this;
 
+        //Test si la géolocalisation est possible sur le navigateur
         if (!navigator.geolocation){
-            $('.location').text("La géolocalisation n'est pas supportée par votre navigateur");
+            $('.location').text("La géolocalisation n'est pas supportée par votre navigateur");                                         //A mettre dans le "script.js"
             return;
         }
 
         function success(position) {
-            var latitude  = position.coords.latitude;
-            var longitude = position.coords.longitude;
-        
-            var url = 'maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key=';
-            that.Request(url, function(response) {
-                $('.location').text(response.results[2].address_components[0].long_name);
+            that.city = new City(position.coords.longitude, position.coords.latitude);
+                    
+            Request(`maps/api/geocode/json?latlng=${that.city.latitude},${that.city.longitude}&key=${key}`, function(response) {
+                let name = response.results[2].address_components[0].long_name;
+                //Affiche le nom de la ville
+                $('.location').text(name);                                                                                              //A mettre dans le "script.js"
+                that.city.name = name;
+                that.city.FetchTooltipWeather();
             });
         }
 
         function error() {
-            $('.location').text("Impossible de retrouver votre localisation.");
+            $('.location').text("Impossible de retrouver votre localisation.");                                                         //A mettre dans le "script.js"
         }
 
-        $('.location').text("Localisation en cours...");
-        navigator.geolocation.getCurrentPosition(success, error);
+        $('.location').text("Localisation en cours...");                                                                                //A mettre dans le "script.js"
+        navigator.geolocation.getCurrentPosition(success, error);   
     }
 
     ShowMap(){
@@ -45,22 +48,7 @@ class Program{
 
     ClosePopup(){
 
-    }
-
-    Request(url, callback, type = 'GET', baseUrl = `https://maps.googleapis.com/`){
-        $.ajax({
-            url: baseUrl + url.split('?')[0],
-            type: type,
-            data: url.split('?')[1],
-            success: data => {
-                callback(data);
-            },
-            error: function(xhr, status, error) {
-                //Affichage du message d'erreur
-                console.log(xhr.status);
-            }
-        });
-    }
+    }    
 }
 
 class City{
@@ -69,7 +57,25 @@ class City{
         this.latitude = latitude
     }
     
-    FetchMeteo(){
-        
+    FetchTooltipWeather(){
+        let that = this
+        Request(`services/json/lat=${this.latitude}lng=${this.longitude}`, function(response){
+            $('.location').attr("title", `Météo de ${that.name} du ${response.fcst_day_0.day_long}`).tooltip('fixTitle').tooltip('show');   //A mettre dans le "script.js"
+        }, 'GET', 'http://prevision-meteo.ch/');
     }
+}
+
+function Request(url, callback, type = 'GET', baseUrl = `https://maps.googleapis.com/`){
+    $.ajax({
+        url: baseUrl + url.split('?')[0],
+        type: type,
+        data: url.split('?')[1],
+        success: data => {
+            callback(data);
+        },
+        error: function(xhr, status, error) {
+            //Affichage du message d'erreur
+            console.log(xhr.status);
+        }
+    });
 }
